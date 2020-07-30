c1b8089db6d1dfb9c17d47365332a86b1ba48243
Ben Ubois <ben@benubois.com>
2020-07-30 19:44:17 +0200

    Proxy support.

--- app/request.rb
+++ app/request.rb
@@ -1,21 +1,28 @@
 class Request
-  attr_reader :uri
 
-  def self.get(uri)
-    http_request = Net::HTTP::Get.new uri.request_uri
-    new(uri, http_request).send(:request)
+  def self.get(url)
+    new(url).send(:request)
   end
 
   private
 
-  def initialize(uri, http_request)
-    @uri = uri
-    @http_request = http_request
+  def initialize(url)
+    @url = url
   end
 
   def request
     http = Net::HTTP.new(uri.host, uri.port)
     http.use_ssl = uri.scheme == "https"
-    http.request(@http_request).body.force_encoding("UTF-8")
+    http.request(Net::HTTP::Get.new uri.request_uri).body.force_encoding("UTF-8")
+  end
+
+  def uri
+    @uri ||= begin
+      if ENV["PROXY_URL"]
+        URI("#{ENV["PROXY_URL"]}?url=#{@url}")
+      else
+        @url
+      end
+    end
   end
 end
